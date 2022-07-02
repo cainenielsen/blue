b4d1c73df7d6e69adb9831610ec315d902575c8a
Caine J Nielsen <caine.nielsen@caineandrebekah.com>
2022-07-02 16:49:12 -0600

    setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="setupBluetooth">setup</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  methods: {
+    async setupBluetooth() {
+      if (!("bluetooth" in navigator)) return;
+      const { bluetooth } = navigator;
+      console.log(bluetooth);
+      const availability = await bluetooth.getAvailability();
+      if (availability === false) return;
+
+      const device = await bluetooth.requestDevice({
+        acceptAllDevices: true
+      });
+      console.log(device.name);
+
+      const devices = await bluetooth.getDevices();
+      console.log(devices);
+    }
   }
-}
+};
 </script>
