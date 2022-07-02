168bc191de470c20317e29ebeca2e126c7d9769e
Caine J Nielsen <caine.nielsen@caineandrebekah.com>
2022-07-02 17:21:51 -0600

    update

---
+++ precache-manifest.c32367d5fb98842cf3c7177e95c8fc35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761790e2d5420a77dbc4cb99f5959e7c",
+    "url": "/blue/index.html"
+  },
+  {
+    "revision": "0ab4fc8cd1d4394999d3",
+    "url": "/blue/js/about.256e99e5.js"
+  },
+  {
+    "revision": "39c8ba10719b3862c0ff",
+    "url": "/blue/js/app.d7f56388.js"
+  },
+  {
+    "revision": "c48d96e2ce7da86026e2",
+    "url": "/blue/js/chunk-vendors.3f3dd339.js"
+  },
+  {
+    "revision": "3f6be5fe74b5e714859b5c9c30bff794",
+    "url": "/blue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blue/robots.txt"
+  }
+]);
